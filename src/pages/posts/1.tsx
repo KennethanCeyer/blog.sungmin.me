449f30e6853915558e05b603a0b62712a7fcca51
Sungmin Han <kenneth@pigno.se>
2023-10-02 01:55:58 +0900

    fix: build errors

--- src/pages/posts/1.tsx
+++ src/pages/posts/1.tsx
@@ -6,6 +6,7 @@ const metadata = {
     "레이크하우스: 데이터 레이크와 데이터 웨어하우스를 병합한 새로운 아키텍처",
   tags: ["LakeHouse", "레이크하우스", "Data Engineering"],
   authors: ["sungmin.han"],
+  createdAt: new Date(Date.UTC(2023, 9, 2, 14, 2)),
 };
 
 const Post: React.FC = () => (
